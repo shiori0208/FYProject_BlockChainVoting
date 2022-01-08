bf76b756ead76893004832ba44451c35c831c0bb
Harsh Agarwal <harsh29092000@gmail.com>
2022-01-08 17:28:54 +0530

    Created the smart contracts.

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,34 +1,94 @@
-/*
- * This is an example of an AssemblyScript smart contract with two simple,
- * symmetric functions:
- *
- * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
- *    user (account_id) who sent the request
- * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
- *    defaulting to "Hello"
- *
- * Learn more about writing NEAR smart contracts with AssemblyScript:
- * https://docs.near.org/docs/develop/contracts/as/intro
- *
- */
+import { Context, logging, PersistentMap } from 'near-sdk-as'
 
-import { Context, logging, storage } from 'near-sdk-as'
+const CandidateURL = new PersistentMap<string, string>("CandidateURL");
+const CandidatePair = new PersistentMap<string, string[]>("CandidatePair");
+const PromptArray = new PersistentMap<string, string[]>("Array of Prompts");
+const voteArray = new PersistentMap<string, i32[]>("Stores Votes");
+const userParticipation = new PersistentMap<string, string[]>("User Participation Record");
 
-const DEFAULT_MESSAGE = 'Hello'
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+// View Methods
+
+export function getUrl(name: string): string{
+  if(CandidateURL.contains(name)) {
+    return CandidateURL.getSome(name);
+  } else {
+    logging.log(`Can't find that user`);
+    return ''
+  }
 }
 
-export function setGreeting(message: string): void {
-  const accountId = Context.sender
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(`Saving greeting "${message}" for account "${accountId}"`)
-  storage.set(accountId, message)
+export function didParticipate(prompt: string, user: string): bool{
+  if(userParticipation.contains(prompt)) {
+    let getArray = userParticipation.getSome(prompt);
+    return getArray.includes(user);
+  } else {
+    logging.log('Prompt not found');
+    return false;
+  }
+}
+
+export function getAllPrompt(): string[]{
+  if(PromptArray.contains('AllArrays')) {
+    return PromptArray.getSome("AllArrays");
+  } else {
+    logging.log('No Prompts found');
+    return [];
+  }
+}
+
+export function getVotes(prompt: string): i32[]{
+  if(voteArray.contains(prompt)) {
+    return voteArray.getSome(prompt);
+  } else {
+    logging.log('Prompt not found for this vote');
+    return [0, 0];
+  }
+}
+
+// Change Methods
+
+export function addUrl(name: string, url: string): void{
+  CandidateURL.set(name, url);
+  logging.log('added url for '+name);
+}
+
+export function addCandidatePair(prompt: string, name1: string, name2: string): void{
+  CandidatePair.set(prompt, [name1, name2]);
+}
+
+export function addVote(prompt: string, index: i32): void{
+  if(voteArray.contains(prompt)) {
+    let tempArray = voteArray.getSome(prompt);
+    let tempVal = tempArray[index];
+    let newVal = tempVal + 1;
+    tempArray[index] = newVal;
+    voteArray.set(prompt, tempArray);
+  } else {
+    let newArray = [0, 0];
+    newArray[index] = 1;
+    voteArray.set(prompt, newArray);
+  }
+}
+
+export function recordUser(prompt: string, user: string): void{
+  if(userParticipation.contains(prompt)) {
+    let tempArray = userParticipation.getSome(prompt);
+    tempArray.push(user);
+    userParticipation.set(prompt, tempArray);
+  } else {
+    userParticipation.set(prompt, [user]);
+  }
+}
+
+export function addToPromptArray(prompt: string): void{
+  logging.log('Added to prompt array.');
+  if(PromptArray.contains("AllArrays")) {
+    let tempArray = PromptArray.getSome("AllArrays");
+    tempArray.push(prompt);
+    PromptArray.set("AllArray", tempArray);
+  } else {
+    PromptArray.set("AllArray", [prompt]);
+  }
+
 }
